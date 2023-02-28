24a2eeb09d65da91c3ccb41802b0ec0733ffeff1
Estevao Pereira <estevao.pereira@codenapp.com>
2023-02-28 18:08:47 -0300

    Doing login

---
+++ Src/PersonalFinances.Web/wwwroot/assets/pages/notification/notification.js
@@ -0,0 +1,42 @@
+function personalFinancesNotify(type, message){
+    $.growl({
+        icon: "fa fa-check",
+        title: "",
+        message: message,
+        url: ''
+    },{
+        element: 'body',
+        type: type,
+        allow_dismiss: true,
+        placement: {
+            from: "top",
+            align: "right"
+        },
+        offset: {
+            x: 30,
+            y: 30
+        },
+        spacing: 10,
+        z_index: 999999,
+        delay: 2500,
+        timer: 1000,
+        url_target: '_blank',
+        mouse_over: false,
+        animate: {
+            enter: "animated fadeInDown",
+            exit: "animated fadeOutDown"
+        },
+        icon_type: 'class',
+        template: '<div data-growl="container" class="alert" role="alert">' +
+        '<button type="button" class="close" data-growl="dismiss">' +
+        '<span aria-hidden="true">&times;</span>' +
+        '<span class="sr-only">Close</span>' +
+        '</button>' +
+        '<span data-growl="icon"></span>' +
+        '<span data-growl="title"></span>' +
+        '<span data-growl="message"></span>' +
+        '<a href="#" data-growl="url"></a>' +
+        '</div>'
+    });
+};
+
